2ba27f0f7158704037657fc91f0207949ed016ff
abrahamseni <aagung@reapit.com>
2022-01-27 11:34:17 +0700

    fill messages page

--- src/components/ui/dashboard/messages.tsx
+++ src/components/ui/dashboard/messages.tsx
@@ -1,10 +1,100 @@
+import { cx } from '@linaria/core'
+import {
+  BodyText,
+  Button,
+  ButtonGroup,
+  elMt12,
+  elWFull,
+  FlexContainer,
+  FormLayout,
+  InputWrapFull,
+  Subtitle,
+  Table,
+  TextArea,
+} from '@reapit/elements'
 import React from 'react'
+import { FormMessage, MessageContainer, ReceivedMessage, SendMessage } from '../__styles__/styles'
 
 const Messages = () => {
   return (
-    <div>
-      <h1>Messages</h1>
-    </div>
+    <>
+      <Subtitle>Messages</Subtitle>
+      <FormMessage>
+        <FormLayout>
+          <InputWrapFull>
+            <TextArea type="text" />
+          </InputWrapFull>
+          <Button intent="success">Send</Button>
+        </FormLayout>
+      </FormMessage>
+      <FlexContainer className={cx(elMt12)}>
+        <Table
+          numberColumns={5}
+          className={cx(elWFull)}
+          rows={[
+            {
+              cells: [
+                {
+                  label: 'From',
+                  value: 'Mr Johnny Corrigan',
+                  icon: 'usernameSystem',
+                  narrowTable: {
+                    showLabel: true,
+                  },
+                },
+                {
+                  label: 'Last message',
+                  value: 'How are you doing today?',
+                  narrowTable: {
+                    showLabel: true,
+                  },
+                },
+                {
+                  label: 'Send Time',
+                  value: '19 Apr 2021',
+                  narrowTable: {
+                    showLabel: true,
+                  },
+                },
+                {
+                  label: 'Status',
+                  value: 'Read',
+                  statusCircleIntent: 'danger',
+                  narrowTable: {
+                    showLabel: true,
+                  },
+                },
+              ],
+              expandableContent: {
+                content: (
+                  <>
+                    <MessageContainer>
+                      <ReceivedMessage>
+                        <BodyText hasGreyText hasNoMargin>
+                          Hi Nina, how are you?
+                        </BodyText>
+                      </ReceivedMessage>
+                    </MessageContainer>
+                    <MessageContainer>
+                      <SendMessage>
+                        <BodyText hasGreyText hasNoMargin>
+                          Hi Nina, how are you?
+                        </BodyText>
+                      </SendMessage>
+                    </MessageContainer>
+                    <ButtonGroup alignment="right">
+                      <Button intent="primary" chevronRight type="submit" onClick={() => {}}>
+                        Send
+                      </Button>
+                    </ButtonGroup>
+                  </>
+                ),
+              },
+            },
+          ]}
+        />
+      </FlexContainer>
+    </>
   )
 }
 
